ea50928a6ecb326400e2577372618d97b66f2223
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-09-03 23:12:06 +0200

    Second test Fail

--- test/todo.controller.test.js
+++ test/todo.controller.test.js
@@ -1,9 +1,17 @@
 const TodoController = require("../src/controller/todo.controller");
+const TodoModel = require("../src/model/todo.model");
+
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
   
   it("Should have a createTodoFunction", () => {
     expect(typeof TodoController.createTodo).toBe("function");
+  });
+
+  it("Should call TodoModel.create", () => {
+    TodoController.createTodo();
+    expect(typeof TodoModel.create).toBeCalled();
   });
 
 });
