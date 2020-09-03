d88a565ce06357047caef2a7327c0485dcc97301
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-09-04 00:16:43 +0200

    Complete CREATE method test case and implementatio

--- test/todo.controller.test.js
+++ test/todo.controller.test.js
@@ -16,18 +16,31 @@ beforeEach( () => {
 
 describe("TodoController.createTodo", () => {
   
+  beforeEach( () => {
+    req.body = newTodo;
+  })
+
   it("Should have a createTodoFunction", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
 
   it("Should call TodoModel.create", () => {
-    let req, res, next;
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
-    next = null;
-    req.body = newTodo;
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo)
   });
 
+  it("Should return 201 response code", () => {
+    TodoController.createTodo(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("Should return json body in response", () => {
+    TodoModel.create.mockReturnValue(newTodo);
+    TodoController.createTodo(req, res, next);
+    expect(res._getJSONwData()).toStrictEqual(newTodo)
+  });
+
 });
+
+
